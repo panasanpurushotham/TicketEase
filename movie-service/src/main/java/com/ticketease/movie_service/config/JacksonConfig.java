1f9794f36c415d72a31ffc1549a5566c76b50c44
6268103 <sai.fedex@fedex.com>
2024-07-09 11:04:21 +0530

    Configurations Added for JMS Template

--- movie-service/src/main/java/com/ticketease/movie_service/config/JacksonConfig.java
+++ movie-service/src/main/java/com/ticketease/movie_service/config/JacksonConfig.java
@@ -1,32 +1,21 @@
 package com.ticketease.movie_service.config;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-
-
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 
-    @Configuration
-    public class JacksonConfig {
-
-        @Bean
-        public ObjectMapper objectMapper() {
-            ObjectMapper objectMapper = new ObjectMapper();
-            objectMapper.registerModule(new JavaTimeModule());
-            objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-            objectMapper.configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true);
-            return objectMapper;
-        }
-
-        @Bean
-        public RestTemplate restTemplate(){
-            return new RestTemplate();
-        }
+@Configuration
+public class JacksonConfig {
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true);
+        objectMapper.registerModule(new JavaTimeModule());
+        return objectMapper;
     }
 
-
+}
